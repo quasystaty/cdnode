4d8b98875d8abc43cd79337ad46160f62fc166c9
Mat <bourgeois.mat@gmail.com>
2022-11-04 16:33:11 -0700

    dynamic height for upgrade test

--- txTest/upgrade.sh
+++ txTest/upgrade.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+HEIGHT=$1
 UPGRADE_NAME="v0.6.0"
 BINARY_DIR=".cdnode"
 CHAIN_ID="oasis-2"
@@ -16,7 +17,7 @@ fi
 
 
 # submit upgrade proposal
-$DAEMON_NAME tx gov submit-proposal software-upgrade $UPGRADE_NAME --title "Upgrade to $UPGRADE_NAME" --description "Upgrade to $UPGRADE_NAME" --deposit 10000000$DENOM --upgrade-height 1500 --from me --chain-id $CHAIN_ID --keyring-backend test --home $DAEMON_HOME --node tcp://localhost:26657 --yes --gas-prices $GAS_PRICE$DENOM --gas auto --gas-adjustment 1.5 --broadcast-mode block
+$DAEMON_NAME tx gov submit-proposal software-upgrade $UPGRADE_NAME --title "Upgrade to $UPGRADE_NAME" --description "Upgrade to $UPGRADE_NAME" --deposit 10000000$DENOM --upgrade-height $HEIGHT --from me --chain-id $CHAIN_ID --keyring-backend test --home $DAEMON_HOME --node tcp://localhost:26657 --yes --gas-prices $GAS_PRICE$DENOM --gas auto --gas-adjustment 1.5 --broadcast-mode block
 
 # vote on the proposal
 $DAEMON_NAME tx gov vote 1 yes --from me --chain-id $CHAIN_ID --keyring-backend test --home $DAEMON_HOME --node tcp://localhost:26657 --yes --gas-prices $GAS_PRICE$DENOM --gas auto --gas-adjustment 1.5 --broadcast-mode block
