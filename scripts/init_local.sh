e100a1830ced41fd59b8f029c513b2fdc4f2f669
Luke Venediger <lukev@lukev.net>
2022-11-27 13:15:58 +0200

    chore(dev env setup): introduce config home variable

--- scripts/init_local.sh
+++ scripts/init_local.sh
@@ -9,6 +9,7 @@
 # but rather to work in a local development environment.
 
 BINARY_HOME="$HOME/.cdnode"
+CONFIG_HOME="$BINARY_HOME/config"
 CHAIN_ID="oasis-2"
 DENOM="unoria"
 GAS_PRICE="0.0025"
@@ -40,21 +41,21 @@ cdnoded gentx me 100000000$DENOM --chain-id oasis-2 --commission-rate 0.1 --comm
 cdnoded collect-gentxs > /dev/null 2>&1
 
 # Edit genesis
-sed -i "s/stake/$DENOM/g" ~/.cdnode/config/genesis.json > /dev/null 2>&1
-sed -i 's/"inflation": "[^"]*"/"inflation": "0\.0"/g' ~/.cdnode/config/genesis.json > /dev/null 2>&1
-sed -i 's/"inflation_rate_change": "[^"]*"/"inflation_rate_change": "0\.0"/g' ~/.cdnode/config/genesis.json > /dev/null 2>&1
-sed -i 's/"inflation_min": "[^"]*"/"inflation_min": "0\.0"/g' ~/.cdnode/config/genesis.json > /dev/null 2>&1
-sed -i 's/"voting_period": "[^"]*"/"voting_period": "60s"/g' ~/.cdnode/config/genesis.json > /dev/null 2>&1
-sed -i 's/"quorum": "[^"]*"/"quorum": "0.000001"/g' ~/.cdnode/config/genesis.json > /dev/null 2>&1
+sed -i "s/stake/$DENOM/g" $CONFIG_HOME/genesis.json > /dev/null 2>&1
+sed -i 's/"inflation": "[^"]*"/"inflation": "0\.0"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
+sed -i 's/"inflation_rate_change": "[^"]*"/"inflation_rate_change": "0\.0"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
+sed -i 's/"inflation_min": "[^"]*"/"inflation_min": "0\.0"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
+sed -i 's/"voting_period": "[^"]*"/"voting_period": "60s"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
+sed -i 's/"quorum": "[^"]*"/"quorum": "0.000001"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
 
 # Edit config.toml to set the block speed to 1s
-sed -i 's/^timeout_commit\ =\ .*/timeout_commit\ =\ \"1s\"/g' ~/.cdnode/config/config.toml > /dev/null 2>&1
+sed -i 's/^timeout_commit\ =\ .*/timeout_commit\ =\ \"1s\"/g' $CONFIG_HOME/config.toml > /dev/null 2>&1
 
 # Edit app.toml to set the minimum gas price
-sed -i "s/^minimum-gas-prices\ =\ .*/minimum-gas-prices\ =\ \"0.0025$DENOM\"/g" ~/.cdnode/config/app.toml > /dev/null 2>&1
+sed -i "s/^minimum-gas-prices\ =\ .*/minimum-gas-prices\ =\ \"0.0025$DENOM\"/g" $CONFIG_HOME/app.toml > /dev/null 2>&1
 
 # Edit app.toml to enable LCD REST server on port 1317 and REST documentation at http://localhost:1317/swagger/
-sed -i 's/^enable\ =\ false/enable\ =\ true/g' ~/.cdnode/config/app.toml > /dev/null 2>&1
-sed -i 's/^swagger\ =\ false/swagger\ =\ true/g' ~/.cdnode/config/app.toml > /dev/null 2>&1
+sed -i 's/^enable\ =\ false/enable\ =\ true/g' $CONFIG_HOME/app.toml > /dev/null 2>&1
+sed -i 's/^swagger\ =\ false/swagger\ =\ true/g' $CONFIG_HOME/app.toml > /dev/null 2>&1
 
 echo -e "\n\nYou can now start your chain with 'cdnoded start'\n"
